7bb75d992ad9f90be6dccda9d7161ea21ffe766d
zazacoder1 <sweetsweet.piechota@gmail.com>
2024-09-22 15:27:21 +0200

    Update dupetools.lua

--- dupetools.lua
+++ dupetools.lua
@@ -1,6 +1,6 @@
 --UI setup
 local plrUI = game:GetService("Players").LocalPlayer.PlayerGui
-
+-- MainUi
 local base = Instance.new("ScreenGui",plrUI)
 local bframe = Instance.new("Frame",base)
 bframe.Size = UDim2.new(0.2,0,0.2,0)
@@ -11,6 +11,8 @@ local dragging = false
 local dragStart = nil
 local startPos = nil
 local UIS = game:GetService("UserInputService")
+local infr = false
+local mv = false
 
 bframe.InputBegan:Connect(function(input)
 	if input.UserInputType == Enum.UserInputType.MouseButton1 then
@@ -38,7 +40,7 @@ UIS.InputEnded:Connect(function(input)
 	end
 end)
 local txtl = Instance.new('TextLabel',bframe)
-txtl.Size = UDim2.new(1,0,0.2)
+txtl.Size = UDim2.new(0.9,0,0.2)
 txtl.BackgroundColor3 = Color3.new(0.388235, 0.388235, 0.388235)
 txtl.Text = "Dupe any items eqquipped! rev:1"
 txtl.BorderSizePixel = 0
@@ -55,6 +57,60 @@ unloadbtn.BackgroundColor3 = Color3.new(1,0,0)
 unloadbtn.Text = 'Unload'
 unloadbtn.Position = UDim2.new(0,0,0.9)
 unloadbtn.BorderSizePixel = 0
+local morebtn = Instance.new('ImageButton',bframe)
+morebtn.Size = UDim2.new(0.2,0,0.2,0)
+Instance.new("UIAspectRatioConstraint",morebtn)
+morebtn.Image = 'rbxassetid://1921781866'
+morebtn.BackgroundColor3 = Color3.new(0.301961, 0.301961, 0.301961)
+morebtn.BorderSizePixel = 0
+morebtn.Position = UDim2.new(0.9,0,0,0)
+Instance.new('UICorner',morebtn).CornerRadius = UDim.new(0,20)
+-- AdditionalUi
+
+local aframe = Instance.new('Frame',bframe)
+aframe.Size = UDim2.new(0.7,0,1,0)
+aframe.BackgroundColor3 = Color3.new(0.223529, 0.223529, 0.223529)
+aframe.BorderSizePixel = 0
+aframe.ZIndex = -1
+local atxt = Instance.new('TextLabel',aframe)
+atxt.Size = UDim2.new(1,0,0.2)
+atxt.BackgroundColor3 = Color3.new(0.388235, 0.388235, 0.388235)
+atxt.Text = "Additional Scripts"
+atxt.BorderSizePixel = 0
+atxt.ZIndex = -1
+local scrframe = Instance.new('ScrollingFrame',aframe)
+scrframe.ZIndex = -1
+scrframe.Size = UDim2.new(1,0,0.8)
+scrframe.Position = UDim2.new(0,0,0.2)
+scrframe.BackgroundColor3 = Color3.new(0.282353, 0.282353, 0.282353)
+scrframe.BorderSizePixel = 0
+local uigrid = Instance.new('UIGridLayout',scrframe)
+uigrid.CellSize = UDim2.new(1,0,0.1)
+
+local style = {
+	-1,
+	UDim2.new(1,0,0.1),
+	Color3.new(0.458824, 0.458824, 0.458824),
+	0
+}
+local scripts = {
+	{'Infinite Yield','https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'},
+	{'Dark Dex V5', 'https://raw.githubusercontent.com/AlterX404/DarkDEX-V5/refs/heads/main/DarkDEX-V5'}
+}
+
+for i,v in pairs(scripts) do
+	local scb = Instance.new('TextButton',scrframe)
+	scb.ZIndex = style[1]
+	scb.Size = style[2]
+	scb.BackgroundColor3 = style[3]
+	scb.BorderSizePixel = style[4]
+	scb.Text = v[1]
+	scb.MouseButton1Up:Connect(function()
+		loadstring(v[2])
+	end)
+end
+
+
 
 -- Scripts
 local backpack = game:GetService("Players").LocalPlayer.Backpack
@@ -64,7 +120,7 @@ btn.MouseButton1Up:Connect(function()
 	local toolInSlot1 = nil
 	game:GetService("Players").LocalPlayer.Character:WaitForChild("Humanoid")
 	local character = game:GetService("Players").LocalPlayer.Character
-	
+
 	-- Loop through all tools in the character (equipped tools)
 	for _, tool in ipairs(character:GetChildren()) do
 		if tool:IsA("Tool") and tool.Parent == character then
@@ -75,8 +131,36 @@ btn.MouseButton1Up:Connect(function()
 	local toolClone = toolInSlot1:Clone()
 	toolClone.CanBeDropped = true
 	toolClone.Parent = backpack
+	print(toolClone)
 end)
 
 unloadbtn.MouseButton1Up:Connect(function()
 	base:Destroy()
+end)
+
+morebtn.MouseButton1Up:Connect(function()
+	local tws =  game:GetService("TweenService")
+	local addtf = tws:Create(aframe,TweenInfo.new(3,Enum.EasingStyle.Sine,Enum.EasingDirection.InOut),{Position = UDim2.new(1,0,0,0)})
+	local addtb = tws:Create(aframe,TweenInfo.new(3,Enum.EasingStyle.Sine,Enum.EasingDirection.InOut),{Position = UDim2.new(0,0,0,0)})
+	
+	local mbtnrf = tws:Create(morebtn,TweenInfo.new(3,Enum.EasingStyle.Sine,Enum.EasingDirection.InOut),{Rotation = 180})
+	local mbtnrb = tws:Create(morebtn,TweenInfo.new(3,Enum.EasingStyle.Sine,Enum.EasingDirection.InOut),{Rotation = 0})
+	
+	if infr == false then
+		addtf:Play()
+		mbtnrf:Play()
+		
+		mv = true
+		addtf.Completed:Wait()
+		mv = false
+		infr = true
+	else
+		addtb:Play()
+		mbtnrb:Play()
+		
+		mv = true
+		addtb.Completed:Wait()
+		mv = false
+		infr = false
+	end
 end)
